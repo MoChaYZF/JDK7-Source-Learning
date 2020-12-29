b0015d32d26649894a1caf51ff4a5ce0551a4652
MoChaYZF <1358192165@qq.com>
2020-12-29 09:05:14 +0800

    Initial commit

---
+++ test/hashmap/HashMapDemo.java
@@ -0,0 +1,18 @@
+package hashmap;
+
+import java.util.HashMap;
+
+/**
+ * 描述 : HashMap7Demo.
+ *
+ * @author : MoCha
+ * @version : v1
+ * @date : 2020-12-29 08:55
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<String, Integer> stringIntegerHashMap = new HashMap<>();
+        stringIntegerHashMap.put("1", 1);
+        stringIntegerHashMap.put("2", 2);
+    }
+}
